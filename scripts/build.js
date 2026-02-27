5a6c3d46116f5dfc8be4d8fecee045e18964e943
rileywallaceriley <67809682+rileywallaceriley@users.noreply.github.com>
2026-02-27 00:42:04 -0500

    Update build.js

--- scripts/build.js
+++ scripts/build.js
@@ -263,6 +263,7 @@ async function fetchAllBandcamp() {
 console.log(’\nScraping Bandcamp…’);
 const browser = await puppeteer.launch({
 headless: ‘new’,
+executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
 args: [’–no-sandbox’,’–disable-setuid-sandbox’,’–disable-dev-shm-usage’,’–disable-gpu’],
 });
 const page = await browser.newPage();
